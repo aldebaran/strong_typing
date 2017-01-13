932a870585dd7ce2844868a853c934865d78a16c
sambrose <sambrose@aldebaran.com>
2017-01-13 17:37:15 +0100

    Add new information in setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,29 @@ CONTAINING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 package_list = find_packages(where=CONTAINING_DIRECTORY)
 
 setup(
-    name='strong-typing-py',
+    name='strong_typing',
     version=open(os.path.join(CONTAINING_DIRECTORY,"strong_typing/VERSION")).read().split()[0],
-    author='Surya Ambrose',
-    author_email='sambrose@softbankrobotics.com',
-    packages=package_list,
-    package_data={"strong_typing":["VERSION"]},
-    url='.',
-    license='LICENSE.txt',
     description='Classes to create strongly typed structures in Python',
     long_description=open(os.path.join(CONTAINING_DIRECTORY,'README.rst')).read(),
-    test_suite="tests",
+    url='https://gitlab.aldebaran.lan/sambrose/py_strong_typing',
+    author='Surya Ambrose',
+    author_email='sambrose@softbankrobotics.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    keywords='static-typing strongly-typed structures',
+    packages=package_list,
     install_requires=[
+        # 'enum34;python_version<"3.4"', not recognized by old setuptools ?
         "enum34 >= 1.0.4"
-    ]
+    ],
+    package_data={
+        "strong_typing":["VERSION", "../README.rst"]
+    }
 )
