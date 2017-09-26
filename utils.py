ee6a2d5e89f9b8ebc9387bf564f0ff4f8deed052
sambrose <sambrose@aldebaran.com>
2017-09-26 14:34:42 +0200

    Add LICENSING

--- utils.py
+++ utils.py
@@ -1,14 +1,40 @@
+# Copyright (c) 2017, Softbank Robotics Europe
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Standard libraries
 import subprocess
-from distutils import log
 
 def get_version_from_tag():
     cmd = "git describe --tags --dirty"
     git_description = subprocess.check_output(cmd.split())
     # Will return the closest tag: e.g. 0.1.0
     # then add -X-SHA-1 where X is the number of commit
-    # between HEAD and that tag, SHA--1 is the current head SHA-1
+    # between HEAD and that tag, SHA-1 is the current head SHA-1
     # e.g: 0.1.0-10-g48b85
     # If HEAD is on the tag, nothing is added
     # Finally, it adds -dirty if changes are not commited
